90a4a012765f13017dfcc0b01f3534d0eebb98c8
Zach Willis <zachmw27@gmail.com>
2023-11-22 18:05:03 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main").remove();
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id' , 'victory');
+
+newHeader.innerHTML = "ZACH is the champion"
